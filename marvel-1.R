6141df6c83a2e57c40f63389e464b7777ccaf388
prem-murari <pm23ace@herts.ac.uk>
2024-12-28 14:34:16 +0530

    Update marvel-1.R
    Optimize data wrangling with dplyr for grouping and summarizing

--- marvel-1.R
+++ marvel-1.R
@@ -16,8 +16,6 @@ head(marvel, 2)
 
 library(ggplot2)
 
-
-library(ggplot2)
 ggplot(marvel, aes(x = Alignment, y = Universe, color = Alignment)) +
   geom_jitter(position = position_jitter(width = 0.3, height = 0.2), size = 3, alpha = 0.6) +  
   labs(
